86f82c03442b9bb75d99866443d1530fb4617be5
lucasescobosa <lucasescobosa@gmail.com>
2022-09-06 21:37:26 -0300

    modifico el sitio para que sea dinamico y reciba datos de la DB

--- Code/routes/products.js
+++ Code/routes/products.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+//Declaración de variables de Multer para subir imagenes
+var storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, 'public/iamges/products');
+    },
+    filename : (req, file, cb) => {
+        cb(null, 'img-' + Date.now() + path.extname(file.originalname));
+    }
+})
+var upload = multer ({storage:storage});
+
+
 const productsController = require('../controllers/productsController')
 
+//Listado de productos
+router.get('/', productsController.index);  //vista index?
+
+//Formulario de creación de productos
+router.get('/create', productsController.create);
+router.post('/', upload.single('image'), productsController.store);
+
+//Detalle de producto
+router.get('/detail/:id', productsController.detail);
+
+//Edicion de producto
+router.get('/:id/edit', productsController.modify);
+router.put('/:id', productsController.update);
+
+//Borrado de producto
+router.delete('/:id', productsController.delete);
+
+//Carro de compras
 router.get('/cart', productsController.cart);
-router.get('/detail', productsController.detail);
-router.get('/modify', productsController.modify);
-
-
-
 
 module.exports = router;
